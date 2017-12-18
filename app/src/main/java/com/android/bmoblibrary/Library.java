f03992bebf8660b66d6249eb03c0a47edd6709c4
Rea <rea.x@iv66.net>
2017-12-18 13:27:26 +0800

    123

--- app/src/main/java/com/android/bmoblibrary/Library.java
+++ app/src/main/java/com/android/bmoblibrary/Library.java
@@ -1,6 +1,10 @@
 package com.android.bmoblibrary;
 
 import android.app.Application;
+import android.text.TextUtils;
+
+import cn.bmob.v3.Bmob;
+import cn.jpush.android.api.JPushInterface;
 
 /**
  * author: Rea.X
@@ -9,16 +13,22 @@ import android.app.Application;
 
 public class Library {
 
-    public static void init(Application app){
+    public static void init(Application app, String bmobKey, boolean isDebug){
         Library.app = app;
+        Library.isDebug = isDebug;
+        JPushInterface.setDebugMode(isDebug);
+        JPushInterface.init(app);
+        if (!TextUtils.isEmpty(bmobKey))
+            Bmob.initialize(app, bmobKey);
     }
 
     private static Application app;
+    private static boolean isDebug;
     public static Application get(){
         return app;
     }
 
     public static boolean isDebug() {
-        return true;
+        return isDebug;
     }
 }
